4f1bc45f9008dc648f3fe4318f17ad5f687cfd6b
Nông Ngọc Hoài <ngochoaitn@gmail.com>
2023-01-12 23:29:54 +0700

    Cập nhật sample c#

--- csharp/GpmLoginApiV2Sample/GpmLoginApiV2Sample/Program.cs
+++ csharp/GpmLoginApiV2Sample/GpmLoginApiV2Sample/Program.cs
@@ -9,15 +9,15 @@ namespace GpmLoginApiV2Sample
         static string apiUrl = "http://127.0.0.1:19995";
         static void Main(string[] args)
         {
-            //GPMLoginAPI api = new GPMLoginAPI(apiUrl);
-            //Console.Write("Profile id: ");
-            //string profileId = Console.ReadLine();
-            //JObject startedResult = api.Start(profileId);
-            //Console.WriteLine($"selenium_remote_debug_address = {startedResult["selenium_remote_debug_address"]}");
-            //Console.WriteLine("Profile stared Enter to exit");
-            //Console.ReadLine();
+            GPMLoginAPI api = new GPMLoginAPI(apiUrl);
+            Console.Write("Profile id: ");
+            string profileId = Console.ReadLine();
+            JObject startedResult = api.Start(profileId);
+            Console.WriteLine($"selenium_remote_debug_address = {startedResult["selenium_remote_debug_address"]}");
+            Console.WriteLine("Profile stared Enter to exit");
+            Console.ReadLine();
 
-            GPMLoginApi_MoreSample.SampleAllApiFunction();
+            // GPMLoginApi_MoreSample.SampleAllApiFunction();
 
             //GPMLoginApi_MoreSample.TestLoginGoogle(); // This is test sample, we not support code for it :(
         }
